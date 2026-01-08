45b791dd04eb94b8a310d19db0339497619bfc6a
jefpoughon-tech <jef.ottavi@gmail.com>
2026-01-08 09:19:45 -0500

    step 6

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion detector")
+
+@app.route("/emotionDetector")
+def emotion_detect():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    
+    formatted_string = "For the given statement, the system response is {}. The dominant emotion is <b>{}</b>.".format(response, response['dominant_emotion'])
+    
+    return formatted_string
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
